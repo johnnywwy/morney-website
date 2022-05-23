684f9a63fa11a045dd00c5149e8bc64a81f4523c
johnnywwy <johnnywwy@gmail.com>
2022-05-23 15:46:44 +0800

    deploy

---
+++ precache-manifest.969e810a09645969582aff92b8ad7b57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7bae74c18f815021083",
+    "url": "/morney-website/css/app.20529fc1.css"
+  },
+  {
+    "revision": "aae811ad0fec0fd8866699af77fe96de",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "c7bae74c18f815021083",
+    "url": "/morney-website/js/app.d35749ff.js"
+  },
+  {
+    "revision": "1ff810bed546ad1d1329",
+    "url": "/morney-website/js/chunk-vendors.e6acfe39.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
